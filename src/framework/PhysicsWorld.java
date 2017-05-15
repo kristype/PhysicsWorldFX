08a269a54d86177521506ea623cee7643d82b792
Kristianw87 <kristianw87@gmail.com>
2017-05-15 22:19:38 +0200

    First running code

--- src/framework/PhysicsWorld.java
+++ src/framework/PhysicsWorld.java
@@ -3,8 +3,6 @@ package framework;
 import java.util.HashMap;
 
 import org.jbox2d.collision.shapes.PolygonShape;
-import org.jbox2d.common.Mat22;
-import org.jbox2d.common.OBBViewportTransform;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.Body;
 import org.jbox2d.dynamics.BodyDef;
@@ -13,7 +11,9 @@ import org.jbox2d.dynamics.Fixture;
 import org.jbox2d.dynamics.World;
 
 import javafx.animation.AnimationTimer;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.geometry.Bounds;
 import javafx.geometry.Point2D;
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
@@ -21,7 +21,7 @@ import shapes.PhysicsRectangle;
 
 public class PhysicsWorld extends Pane {
 	
-	private World world = new World(new Vec2());
+	private World world = new World(new Vec2(1.0f, 1.0f));
 	
 	HashMap<Node, Body> nodeBodyMap = new HashMap<Node, Body>();
 	
@@ -32,8 +32,8 @@ public class PhysicsWorld extends Pane {
 		public void handle(long now) {
 			if (last >= 0) {
 				long milliseconds = (now - last) / 1_000_000;
-				if (milliseconds > 1000.0 / 60.0 && world != null) {
-					world.step(((float) milliseconds) / 1000.0f, 8, 3);
+				if (milliseconds > 1000.0 / 60.0 && getWorld() != null) {
+					getWorld().step(((float) milliseconds) / 1000.0f, 8, 3);
 					last = now;
 				}
 			} else {
@@ -49,8 +49,8 @@ public class PhysicsWorld extends Pane {
 		public void handle(long now) {
 			if (last >= 0) {
 				long milliseconds = (now - last) / 1_000_000;
-				if (milliseconds > 1000.0 / 60.0 && world != null) {
-					
+				if (milliseconds > 1000.0 / 60.0 && getWorld() != null) {
+					UpdateRenderPosition();
 					last = now;
 				}
 			} else {
@@ -58,54 +58,87 @@ public class PhysicsWorld extends Pane {
 			}
 		}
 	};
-
-	private OBBViewportTransform viewportTransform;
+	
+	private void UpdateRenderPosition() {
+		updatePositions();
+	}
 	
 	public PhysicsWorld(){
-		OBBViewportTransform obb = new OBBViewportTransform();
+		
 		float scale = (float) 40;
-		obb.setTransform(new Mat22(scale, 0.0f, 0.0f, scale));
-		obb.setCenter((float) (this.getWidth() / 2), (float) (this.getHeight() / 2));
-		obb.setExtents((float) (this.getWidth() / 2), (float) (this.getHeight() / 2));
-		obb.setYFlip(true);
-		this.viewportTransform = obb;
 		
 		ObservableList<Node> children = this.getChildren();
-		for (Node node : children) {
-			if (node.getClass().isInstance(PhysicsRectangle.class)){
-				BodyDef bodyDefinition = new BodyDef();
-				bodyDefinition.type = BodyType.DYNAMIC;
-				
-				Body body = this.world.createBody(bodyDefinition);
-				PolygonShape shape = new PolygonShape();
-				shape.setAsBox(1f, 1f);
-				Fixture fixture = body.createFixture(shape,1f);
-				this.nodeBodyMap.put(node, body);
-			}
-		}
-		this.worldTimer.start();
-		this.renderTimer.start();
+		children.addListener((ListChangeListener.Change<? extends Node> c) -> {
+			while (c.next()) {
+		        if (c.wasAdded()) {
+		            for(Node node : c.getAddedSubList()) {
+		  
+						BodyDef bodyDefinition = new BodyDef();
+						bodyDefinition.type = BodyType.DYNAMIC;
+						Bounds bounds = node.getLayoutBounds();
+						float hWidth = (float) bounds.getWidth() / 2f;
+						float hHeight = (float) bounds.getHeight() / 2f;
+						
+						float x = (float)((node.getLayoutX() + hWidth )  / scale);
+						float y = (float)((node.getLayoutY() + hHeight)  / scale);
+						bodyDefinition.position = new Vec2(x, y);
+						bodyDefinition.linearDamping = 0f;
+						
+						
+						Body body = this.getWorld().createBody(bodyDefinition);
+						PolygonShape shape = new PolygonShape();
+						shape.setAsBox(hWidth / scale, hHeight / scale);
+						Fixture fixture = body.createFixture(shape, 1f);
+						this.nodeBodyMap.put(node, body);
+						
+						((PhysicsRectangle)node).setBody(body);
+						
+						System.out.println("Body added to world");
+						System.out.println(body.getPosition().x);
+						System.out.println(body.getPosition().y);
+		            }
+		        }
+		    }
+		});
+		
+
+	}
+	
+	public void StartWorld(){
+		System.out.println("Starting world");
+		worldTimer.start();
+	}
+	
+	public void Start(){
+		System.out.println("Starting world");
+		worldTimer.start();
+		renderTimer.start();
+	}
+	
+	public void Stop(){
+		System.out.println("Stopping world");
+		worldTimer.stop();
+		renderTimer.stop();
 	}
 	
 	public void updatePositions(){
-		for (Node node : this.getChildren()) {
-			Body body = nodeBodyMap.get(node);
-			Point2D nodePosition = this.world2fx(body.getPosition().x, body.getPosition().y);
-			node.setLayoutX(nodePosition.getX());
-			node.setLayoutY(nodePosition.getY());
+		for (Node node : this.nodeBodyMap.keySet()) {
+			Body body = this.nodeBodyMap.get(node);
+			Vec2 position = body.getPosition();
+			Point2D nodePosition = this.world2fx(position.x, position.y);
+			Bounds boundsInLocal = node.getBoundsInLocal();
+			node.setLayoutX(nodePosition.getX() - (boundsInLocal.getWidth() / 2));
+			node.setLayoutY(nodePosition.getY() - (boundsInLocal.getHeight() / 2));
 			double fxAngle = (- body.getAngle() * 180 / Math.PI) % 360;
 			node.setRotate(fxAngle);
 		}
 	}
 	
-	private Vec2 world2fxTemp1 = new Vec2(), world2fxTemp2 = new Vec2();
-	
 	public Point2D world2fx(double x, double y) {
-		Vec2 result = world2fxTemp1;
-		world2fxTemp2.set((float) x, (float) y);
-		viewportTransform.getWorldToScreen(new Vec2((float) x, (float) y), result);
-		return new Point2D(result.x, result.y);
+		return new Point2D(x * (double)40, y * (double)40);
 	}
 
-
+	public World getWorld() {
+		return world;
+	}
 }
--- src/shapes/PhysicsRectangle.java
+++ src/shapes/PhysicsRectangle.java
@@ -1,7 +1,18 @@
 package shapes;
 
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+
 import javafx.scene.shape.Rectangle;
 
 public class PhysicsRectangle extends Rectangle {
-	
+	private Body body;
+
+	public void setBody(Body body) {
+		this.body = body;
+	}
+
+	public void SetSpeed(float vx, float vy) {
+		body.setLinearVelocity(new Vec2(vx, vy));
+	}
 }
